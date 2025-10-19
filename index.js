6edf9a5a7fbad685a96256442b99085501fa0c0d
Dusan-Zalar <127787798+Dusan-Zalar@users.noreply.github.com>
2025-10-19 10:54:00 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const http = require('http');
+
+function reqListener(req, res) {
+    const url = req.url;
+
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>supabase</title></head>');
+        res.write('<body><h1>Hello</p>');
+        res.write('</html>');
+        res.end();
+    } else {
+	res.end('404 not found')
+    }
+
+}
+const { createClient } = require('@supabase/supabase-js');
+// import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://https://vnvqjhcoteauvxrccdlo.supabase.co';
+const supabaseKey = `Public anon key`
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+//const supabaseUrl = 'https://vnvqjhcoteauvxrccdlo.supabase.co';
+//const supabaseKey = process.env.SUPABASE_KEY;
+//const supabase = createClient(supabaseUrl, supabaseKey);
+
+const server = http.createServer(reqListener);
+
+server.listen(3000);
